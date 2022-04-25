4d945d0ace2bde9173250456250475b62f955ea2
Imran Hossen <freelancerimran81@gmail.com>
2022-04-25 15:57:34 +0600

    done

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+    next();
+}
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yauiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -24,12 +41,12 @@ async function run() {
 
 
         // SERVICE API
-        app.post('/login', async(req, res)=>{
+        app.post('/login', async (req, res) => {
             const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1d'
             });
-            res.send({accessToken});
+            res.send({ accessToken });
         })
 
 
@@ -63,12 +80,17 @@ async function run() {
 
         // Order Collection API
 
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = {email: email};
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }else{
+                
+            }
         })
 
         app.post('/order', async (req, res) => {
